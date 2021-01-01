8e46569117458c149ce49e78dca9638d5ebec134
Ludvig Ström <ludvigstrom@gmail.com>
2021-01-01 18:52:38 +0100

    Fixed particles

--- snakeman/Assets/Scripts/playerMovement.cs
+++ snakeman/Assets/Scripts/playerMovement.cs
@@ -14,19 +14,19 @@ public class playerMovement : MonoBehaviour {
     private Vector2 movement_vector;
 
     private ParticleSystem ps;
-    
-    // Use this for initialization
+
+    private void Awake(){
+        anim = GetComponent<Animator>();
+    }
+
     void Start () {
         rbody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
         ps = GetComponentInChildren<ParticleSystem>();
-
         lastPosition = this.transform.position;
 	}
 
+    public void OnMove(InputValue value){
 
-    public void OnMove(InputValue value)
-    {
         movement_vector = value.Get<Vector2>();
 
         if (movement_vector != Vector2.zero)
@@ -34,29 +34,22 @@ public class playerMovement : MonoBehaviour {
             anim.SetBool("isWalking", true);
             anim.SetFloat("input_x", movement_vector.x);
             anim.SetFloat("input_y", movement_vector.y);
-        }
-        else
-        {
+        } else{
             anim.SetBool("isWalking", false);
         }
+
     }
 
-    void Update()
-    {
+    void Update(){
         addParticleFx();
     }
 
     void FixedUpdate() {
-
-        movement_vector *= (Mathf.Abs(movement_vector.x) == 1 && Mathf.Abs(movement_vector.y) == 1) ? .7f : 1;
-
         rbody.AddForce(movement_vector * Time.deltaTime * speed);
-
         lastPosition = this.transform.position;
     }
 
-    public GameObject GetPlayerObject()
-    {
+    public GameObject GetPlayerObject(){
         return this.gameObject;
     }
 
@@ -76,15 +69,11 @@ public class playerMovement : MonoBehaviour {
         }
     }
 
-
     private bool sliding()
     {
-        if (movement_vector == Vector2.zero && lastPosition != this.transform.position)
-        {
+        if (movement_vector == Vector2.zero && lastPosition != this.transform.position){
             return true;
-        }
-        else
-        {
+        }else{
             return false;
         }
     }
